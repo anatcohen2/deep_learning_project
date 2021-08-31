3bf3d14da4d5af3b4f8745792c8944b7db7e25d5
jaron1990 <jaron1990@gmail.com>
2021-08-31 19:09:23 +0300

    committing dataset class

---
+++ chexpert_dataset.py
@@ -0,0 +1,112 @@
+# from data_process import remove_unknown
+import pandas as pd
+import numpy as np
+from torch.utils.data import Dataset
+import os
+from PIL import Image
+
+class CheXpertDataset(Dataset):
+    def __init__(
+            self,
+            path_to_images,
+            fold,
+            include_uncertainty=False,
+            transform=None,
+            sample=0,
+            finding="any",
+            starter_images=False):
+
+        self.transform = transform
+        self.path_to_images = path_to_images
+        self.df = pd.read_csv(f"{path_to_images}/{fold}.csv")
+
+        if(starter_images):
+            starter_images = pd.read_csv("starter_images.csv")
+            self.df=pd.merge(left=self.df,right=starter_images, how="inner",on="Image Index")
+
+        if(include_uncertainty == False):
+            self.df = self.remove_unknown(self.df)
+            
+        # can limit to sample, useful for testing
+        # if fold == "train" or fold =="val": sample=500
+        if(sample > 0 and sample < len(self.df)):
+            self.df = self.df.sample(sample)
+
+        if not finding == "any":  # can filter for positive findings of the kind described; useful for evaluation
+            if finding in self.df.columns:
+                if len(self.df[self.df[finding] == 1]) > 0:
+                    self.df = self.df[self.df[finding] == 1]
+                else:
+                    print("No positive cases exist for "+LABEL+", returning all unfiltered cases")
+            else:
+                print("cannot filter on finding " + finding +
+                      " as not in data - please check spelling")
+
+        self.df = self.df.set_index("Path")
+        self.PRED_LABEL = [
+            'No Finding',
+            'Enlarged Cardiomediastinum',
+            'Cardiomegaly',
+            'Lung Opacity',
+            'Lung Lesion',
+            'Edema',
+            'Consolidation',
+            'Pneumonia',
+            'Atelectasis',
+            'Pneumothorax',
+            'Pleural Effusion',
+            'Pleural Other',
+            'Fracture',
+            'Support Devices']
+        RESULT_PATH = "results/"
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+
+        image = Image.open(self.df.index[idx])
+        image = image.convert('RGB')
+
+        label = np.zeros(len(self.PRED_LABEL), dtype=int)
+        for i in range(0, len(self.PRED_LABEL)):
+             # can leave zero if zero, else make one
+            if(self.df[self.PRED_LABEL[i].strip()].iloc[idx].astype('int') >= -1):
+                label[i] = self.df[self.PRED_LABEL[i].strip()
+                                   ].iloc[idx].astype('int')
+
+        if self.transform:
+            image = self.transform(image)
+
+        return (image, label,self.df.index[idx])
+
+    def remove_unknown(self, df):
+        anomalies = ['Enlarged Cardiomediastinum', 'Cardiomegaly', 'Lung Opacity',
+            'Lung Lesion', 'Edema', 'Consolidation', 'Pneumonia', 'Atelectasis',
+            'Pneumothorax', 'Pleural Effusion', 'Pleural Other', 'Fracture']
+
+
+        idxs = ~((df[anomalies] == -1).any(axis=1))
+        new_df = df[idxs]
+        return new_df
+
+
+# dataset_train = CheXpertDataset(
+#         path_to_images='CheXpert-v1.0-small',
+#         fold='train',
+#         include_uncertainty=True)
+
+# dataset_train_filtered = CheXpertDataset(
+#         path_to_images='CheXpert-v1.0-small',
+#         fold='train',
+#         include_uncertainty=False)
+
+# dataset_valid = CheXpertDataset(
+#         path_to_images='CheXpert-v1.0-small',
+#         fold='valid',
+#         include_uncertainty=True)
+
+# dataset_valid_filtered = CheXpertDataset(
+#         path_to_images='CheXpert-v1.0-small',
+#         fold='valid',
+#         include_uncertainty=False)
